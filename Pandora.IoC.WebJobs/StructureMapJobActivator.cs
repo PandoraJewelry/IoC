9e335cef0f467af19f14fa5ab08bb57b06433e33
mark newman <mark.newman@pandora.net>
2016-03-15 19:04:41 -0400

    missing file

---
+++ Pandora.IoC.WebJobs/StructureMapJobActivator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.WebJobs.Host;
+using StructureMap;
+using System;
+
+namespace Pandora.IoC.WebJobs
+{
+    public class StructureMapJobActivator : IJobActivator
+    {
+        #region fields
+        private IContainer _container;
+        #endregion
+
+        public StructureMapJobActivator(IContainer container)
+        {
+            if (container == null)
+                throw new ArgumentNullException(nameof(container));
+
+            _container = container;
+        }
+
+        public T CreateInstance<T>()
+        {
+            return _container.GetInstance<T>();
+        }
+    }
+}
